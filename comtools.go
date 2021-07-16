904704389168f96ad3295bf75139ebed30075fa5
yunzhongmanbu <3088698508@qq.com>
2021-07-16 19:46:38 +0800

    master

---
+++ comtools.go
@@ -0,0 +1,29 @@
+package comtools
+
+import (
+	"log"
+	"net"
+	"os"
+	"strings"
+	"time"
+)
+
+func GetNowTimeStr() string {
+	return time.Unix(time.Now().Unix(), 0).Format("2006-01-02 15:04:05")
+}
+func GetHostName() string {
+	name, _ := os.Hostname()
+	return name
+}
+func GetLocalIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:53")
+	if err != nil {
+		log.Printf("get local addr err:%v\n", err)
+		return ""
+	} else {
+		localIP := strings.Split(conn.LocalAddr().String(), ":")[0]
+		conn.Close()
+		return localIP
+	}
+
+}
